51854964cd18cba5848bad8cd8b10e660b526361
Christophe1 <harris.christophe@gmail.com>
2018-03-09 23:19:30 +0000

    recyclerview working for searching categories

---
+++ main/java/com/example/chris/recyclerview_autocompletetextview2/Contact.java
@@ -0,0 +1,26 @@
+package com.example.chris.recyclerview_autocompletetextview2;
+
+/**
+ * Created by Chris on 08/03/2018.
+ */
+
+public class Contact {
+    String cat_name;
+  //  String image;
+   // String phone;
+
+    public Contact() {
+    }
+
+    public String getName() {
+        return cat_name;
+    }
+
+   // public String getImage() {
+  //      return image;
+  //  }
+
+    //public String getPhone() {
+     //   return phone;
+   // }
+}
